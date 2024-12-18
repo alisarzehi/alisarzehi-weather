5a03039803dc7962f1704b3566e83120a0f11524
alisarzehi <ali.srir@gmail.com>
2024-12-18 09:31:17 +0330

    delete ezafe 2

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,9 +1,6 @@
-import React from "react";
-
 function Background() {
   return (
     <>
-      {" "}
       <div className="absolute inset-0 z-0">
         <div className="h-screen bg-bottom bg-cover transform brightness-50 hue-rotate-90 saturate-50 bg-[url('https://images.tech.co/wp-content/uploads/2015/06/weather.jpg')]" />
       </div>
